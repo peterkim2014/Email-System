f68692e72b2f077b3b1ee15456c940432cdc0ba9
peterkim2014 <peterkim2014@gmail.com>
2023-02-14 18:32:27 -0500

    added smtp server

---
+++ send_mail.py
@@ -0,0 +1,15 @@
+import smtplib
+
+username="program.python.test@gmail.com"
+password = "hfzuhitpywagszzg"
+
+def send_mail(customer, company, to_emails=None):
+    assert isinstance(to_emails, list)
+
+    # login smtp server
+    server = smtplib.SMTP(host="smtp.gmail.com", port=587)
+    server.ehlo()
+    server.starttls()
+    server.login(username, password)
+
+    server.quit()
